4f61dfc269bea30a52ad5a8038ea7d2c3e21dc7f
AlexTar65 <125300751+AlexTar65@users.noreply.github.com>
2023-03-24 11:14:53 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -19,3 +19,17 @@ yes_no_kb_builder.row(button_yes, button_no, width=2)
 yes_no_kb = yes_no_kb_builder.as_markup(
                                 one_time_keyboard=True,
                                 resize_keyboard=True)
+# ------- Создаем игровую клавиатуру без использования билдера -------
+
+# Создаем кнопки игровой клавиатуры
+button_1: KeyboardButton = KeyboardButton(text=LEXICON_RU['rock'])
+button_2: KeyboardButton = KeyboardButton(text=LEXICON_RU['scissors'])
+button_3: KeyboardButton = KeyboardButton(text=LEXICON_RU['paper'])
+
+# Создаем игровую клавиатуру с кнопками "Камень 🗿",
+# "Ножницы ✂" и "Бумага 📜" как список списков
+game_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+                                    keyboard=[[button_1],
+                                              [button_2],
+                                              [button_3]],
+                                    resize_keyboard=True)
